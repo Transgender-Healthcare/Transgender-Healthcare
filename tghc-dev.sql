6d62f8a0d7367f4e80b170fb477884ad93ec2d22
Paige Julianne Sullivan <paige@paigejulianne.com>
2024-07-26 12:15:21 +0000

    Symfony changes

--- tghc-dev.sql
+++ tghc-dev.sql
@@ -17,20 +17,16 @@ CREATE TABLE `location` (
   `meta` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`meta`)),
   `place_name` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `location` VALUES
-(1,'[]','Your House');
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `provider` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `display_name` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `provider` VALUES
-(1,'Dr. Boo Boo');
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `provider_attribute` (
@@ -40,8 +36,6 @@ CREATE TABLE `provider_attribute` (
   UNIQUE KEY `provider_id` (`provider_id`,`attribute_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `provider_attribute` VALUES
-(1,'PreferredName','[\"Ms. BooBoo, MD, PhD\"]');
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `provider_location` (
@@ -51,5 +45,3 @@ CREATE TABLE `provider_location` (
   KEY `location_id` (`location_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `provider_location` VALUES
-(1,1);
